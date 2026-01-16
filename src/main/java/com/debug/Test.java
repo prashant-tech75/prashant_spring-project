acde8ae077d42cd23491426454b7c854720827f0
prashant-tech75 <prashant.raj.sahu75@gmail.com>
2026-01-16 19:00:17 +0530

    spring project commit

---
+++ src/main/java/com/debug/Test.java
@@ -0,0 +1,23 @@
+package com.debug;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Trial of debugging");
+        int[] myarray ={2,5,7,81};
+        int sum=getSum(myarray);
+        System.out.println(sum);
+
+
+    }
+    public static int getSum(int arr[]){
+        int s=0;
+        for(int i=0;i<arr.length;i++){
+            s=s+arr[i];
+
+        }
+        s=s+3;
+        return s;
+    }
+
+
+}
